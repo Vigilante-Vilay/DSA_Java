cb03a5033fbfd5b9cc4551ce843bb150fb4fb89b
Vilay <vilayaggarwal48@gmail.com>
2024-07-06 08:17:40 +0530

    First Queue Program

---
+++ Queue_Array.java
@@ -0,0 +1,68 @@
+//Queue is a FIFO type of data structure
+package DSA_Java;
+
+public class Queue_Array {
+    static class Queue {
+        static int arr[];
+        static int size;
+        static int rear = -1; // No need to define front as it starts from 0 only
+
+        Queue(int n) {
+            this.size = n;
+            arr = new int[n];
+        }
+
+        public static boolean isEmpty() {
+            if (rear == -1) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        // Enqueue - Adding elements to a queue
+        public static void add(int data) {
+            if (rear == size - 1) {
+                System.out.println("Full Queue");
+                return;
+            }
+            rear++;
+            arr[rear] = data;
+        }
+
+        // Dequeue - Removing elements from a queue
+        public static int remove() {
+            if (isEmpty()) {
+                System.out.println("Empty Queue");
+                return -1;
+            }
+            int front = arr[0];
+            for (int i = 0; i < rear; i++) { // Shifting elements one place backwards to fill up the spaces created
+                arr[i] = arr[i + 1];
+            }
+            rear--;
+            return front;
+        }
+
+        // Peek - Take a look at whats in the front of the queue
+        public static int peek() {
+            if (isEmpty()) {
+                System.out.println("Empty Queue");
+                return -1;
+            }
+            return arr[0];
+        }
+    }
+
+    public static void main(String args[]) {
+        Queue q = new Queue(5);
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        while (q.isEmpty() == false) {
+            System.out.println(q.peek());
+            q.remove();
+        }
+    }
+
+}
